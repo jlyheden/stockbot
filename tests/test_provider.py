b7b575cd77064c71688fa3aa5a1a114676036d4f
Johan Lyheden <github-johan@lyheden.com>
2025-03-31 23:12:53 +0200

    Rank sto tickers higher

--- tests/test_provider.py
+++ tests/test_provider.py
@@ -420,13 +420,13 @@ class TestYahooQueryService(unittest.TestCase):
         self.assertEquals("Didn't find anything", str(result))
 
     @vcr.use_cassette('mock/vcr_cassettes/yahoo/quote/microsoft.yaml')
-    def test_get_existing_company(self):
+    def test_get_existing_company_1(self):
         text = "microsoft"
         result = self.service.get_quote(text)
         self.assertRegexpMatches(str(result), "^Name: Microsoft Corporation, Price: [0-9\.]+, Low Price: [0-9\.]+, High Price: [0-9\.]+, Percent Change 1 Day: [0-9\.\-]+, Market: us_market, Chart: https://finance.yahoo.com/chart/MSFT, Update Time: [0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}")
 
-    @vcr.use_cassette('mock/vcr_cassettes/yahoo/quote/bytedance.yaml')
-    def test_get_existing_company(self):
-        text = "bytedance"
+    @vcr.use_cassette('mock/vcr_cassettes/yahoo/quote/investor.yaml')
+    def test_get_existing_company_2(self):
+        text = "investor"
         result = self.service.get_quote(text)
-        self.assertEqual("Didn't find anything", str(result))
+        self.assertRegexpMatches(str(result), "^Name: Investor AB ser. B, Price: [0-9\.]+, Low Price: [0-9\.]+, High Price: [0-9\.]+, Percent Change 1 Day: [0-9\.\-]+, Market: se_market, Chart: https://finance.yahoo.com/chart/INVE-B.ST, Update Time: [0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}")
